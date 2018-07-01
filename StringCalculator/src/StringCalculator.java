38cb30c73adf33e382098965c34d06d9a0994412
Gyarmati Krisztián <steel765@gmail.com>
2018-07-01 09:33:02 +0200

    Elláttam fejléccel, megyek reggelizni.

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -1,6 +1,9 @@
 
 public class StringCalculator {
-
+/**
+ * A programhoz a tutorial ez volt: https://www.youtube.com/watch?v=cdsMIX9gB94
+ * 
+ */
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 	}
